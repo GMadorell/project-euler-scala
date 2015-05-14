68f45b6b5298842fcf1f2a78db02919b2ab06730
Skabed <gmadorell@gmail.com>
2015-05-14 23:38:39 +0200

    Added a more optimized solution to smallest multiple.

--- src/main/scala/solutions/AAE_SmallestMultiple/SmallestMultiple.scala
+++ src/main/scala/solutions/AAE_SmallestMultiple/SmallestMultiple.scala
@@ -1,6 +1,6 @@
 package solutions.AAE_SmallestMultiple
 
-import math.Multiple
+import math.{Primes, Multiple}
 
 object SmallestMultiple extends App {
   /**
@@ -12,7 +12,7 @@ object SmallestMultiple extends App {
   val endRange = 20
 
   /*
-   * The idea of the solution is to take all the numbers that are evenly divisible by the last number of the
+   * The idea of this solution is to take all the numbers that are evenly divisible by the last number of the
    * range and check whether they are also evenly divisible by all the others, starting from the second
    * biggest number. This way we skip having to check a lot of numbers.
    */
@@ -24,4 +24,27 @@ object SmallestMultiple extends App {
     .next()
 
   println("Solution to 'Smallest Multiple': " + solution)
+
+  /*
+   * The idea behind the following more optimized solution is to find the prime factors
+   * of each number of the interval, and store the maximum count of each prime number
+   * required to do the prime factors.
+   *
+   * Then, we just elevate each prime to its count and multiply the results.
+   */
+  var primeFactorsMap = collection.mutable.Map[Int, Int]() withDefaultValue 0
+  for (i <- 2 to 21) {
+    val primeDecompositionCountMap = Primes.primeFactors(i)
+      .foldLeft(Map[Int, Int]() withDefaultValue 0)((map, n) => map + (n -> (map(n) + 1)))
+    primeDecompositionCountMap.foreach {
+      case(prime, count) => primeFactorsMap += (prime -> Math.max(count, primeFactorsMap(prime)))
+    }
+  }
+
+  val primeFactorsSolution =
+    primeFactorsMap.foldLeft(1) { case (accumulator, (prime, count)) => accumulator * Math.pow(prime, count).toInt }
+
+  println("Solution to 'Smallest Multiple' with prime factors: " + primeFactorsSolution)
+
+  require(solution == primeFactorsSolution)
 }
